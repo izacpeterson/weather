e3a7849a7344d486d5abed3981a38b5b825a7a2a
izacpeterson <izacpeterson@gmail.com>
2023-04-19 00:42:26 -0600

    ui  improvements

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -48,54 +48,61 @@
   </div>
 {/if} -->
 
-<main class="flex justify-evenly flex-wrap">
-  <section class="flex flex-col p-4 items-center">
-    <h2 class="text-2xl">Current Weather</h2>
-    <!-- <img src={localWeather.img} alt={localWeather.description} /> -->
-    <span class="text-8xl">{localWeather.temp}&deg;</span>
-    <h3 class="text-xl capitalize">{localWeather.description}</h3>
-    <!-- <span>Feels Like: {localWeather.feels_like}&deg;</span> -->
-    <span>Humidity: {localWeather.humidity}%</span>
-    <!-- <span>Wind Speed: {localWeather.wind_speed} mph @ {localWeather.wind_deg}&deg;</span>
+{#if localWeather.temp}
+  <main class="flex justify-evenly flex-wrap p-4">
+    <section class="flex flex-col pb-4 items-center">
+      <h2 class="text-2xl">Current Weather</h2>
+      <!-- <img src={localWeather.img} alt={localWeather.description} /> -->
+      <span class="text-8xl">{Math.round(localWeather.temp)}&deg;</span>
+      <h3 class="text-xl capitalize">{localWeather.description}</h3>
+      <!-- <span>Feels Like: {localWeather.feels_like}&deg;</span> -->
+      <span>Humidity: {localWeather.humidity}%</span>
+      <!-- <span>Wind Speed: {localWeather.wind_speed} mph @ {localWeather.wind_deg}&deg;</span>
     <span>Cloud Coverage: {localWeather.clouds}%</span>
     <span class="text-xs">
       {coords.latitude}&deg; {coords.longitude}&deg;
     </span> -->
-  </section>
-  <!-- <section class="flex flex-col p-4 items-center">
+    </section>
+    <!-- <section class="flex flex-col p-4 items-center">
     <h2 class="text-2xl">Home Weather</h2>
   </section> -->
-  <section class="w-full">
-    <h2>Hourly Forecast</h2>
-    <div class="flex items-center overflow-x-auto">
-      {#each hourly as hour, index}
-        {#if index <= 12}
-          <div class="m-2 flex flex-col items-center justify-center text-sm">
-            <span>{new Date(parseInt(String(hour.dt) + "000")).toLocaleTimeString().split(":")[0]}{new Date(parseInt(String(hour.dt) + "000")).toLocaleTimeString().split(" ")[1]}</span>
-            <img src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}.png`} />
-            <span>{Math.round(hour.temp)}&deg;</span>
-            <!-- <span>{hour.weather[0].main}</span> -->
+    <section class="w-full pb-4">
+      <h2>Hourly Forecast</h2>
+      <div class="flex items-center overflow-x-auto">
+        {#each hourly as hour, index}
+          {#if index <= 12}
+            <div class="m-2 flex flex-col items-center justify-center text-sm">
+              <span>{new Date(parseInt(String(hour.dt) + "000")).toLocaleTimeString().split(":")[0]}{new Date(parseInt(String(hour.dt) + "000")).toLocaleTimeString().split(" ")[1]}</span>
+              <img src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}.png`} />
+              <span>{Math.round(hour.temp)}&deg;</span>
+              <!-- <span>{hour.weather[0].main}</span> -->
+            </div>
+          {/if}
+        {/each}
+      </div>
+    </section>
+    <section class="w-full">
+      <h2>Daily Forecast</h2>
+      <div class="flex flex-col">
+        {#each daily as day}
+          <div class="flex items-center justify-evenly">
+            <span>{new Date(parseInt(String(day.dt) + "000")).toLocaleDateString()}</span>
+            <img src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`} />
+            <span>High: {Math.round(day.temp.max)}&deg;</span>
+            <span>Low: {Math.round(day.temp.min)}&deg;</span>
+            <!-- <span>{day.weather[0].main}</span> -->
           </div>
-        {/if}
-      {/each}
-    </div>
-  </section>
-  <section class="w-full">
-    <h2>Daily Forecast</h2>
-    <div class="flex flex-col">
-      {#each daily as day}
-        <div class="flex items-center justify-evenly">
-          <span>{new Date(parseInt(String(day.dt) + "000")).toLocaleDateString()}</span>
-          <img src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`} />
-          <span>High: {Math.round(day.temp.max)}&deg;</span>
-          <span>Low: {Math.round(day.temp.min)}&deg;</span>
-          <!-- <span>{day.weather[0].main}</span> -->
-        </div>
-      {/each}
-    </div>
-  </section>
-</main>
+        {/each}
+      </div>
+    </section>
+  </main>
 
-<!-- <pre>
+  <!-- <pre>
     {JSON.stringify(daily[0], null, 2)}
 </pre> -->
+{:else}
+  <div class="flex flex-col items-center">
+    <h2 class="text-2xl">Loading...</h2>
+    <!-- <img src="https://media.giphy.com/media/3oEjI6SIIHBdRxXI40/giphy.gif" /> -->
+  </div>
+{/if}
