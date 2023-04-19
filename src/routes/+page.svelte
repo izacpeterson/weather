291cb7234f030a52f58a29377011c08e8650db7e
izacpeterson <izacpeterson@gmail.com>
2023-04-19 00:23:26 -0600

    ui update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
   let coords = {};
   let localWeather = {};
   let hourly = [];
+  let daily = [];
 
   function getCoords() {
     return new Promise((resolve, reject) => {
@@ -28,6 +29,7 @@
     alerts = weather.alerts;
 
     hourly = weather.hourly;
+    daily = weather.daily;
   }
 
   onMount(() => {
@@ -35,7 +37,7 @@
   });
 </script>
 
-{#if alerts.length > 0}
+<!-- {#if alerts.length > 0}
   <div class="flex flex-col items-center bg-error">
     {#each alerts as alert}
       <span class="text-error-content"> {alert.event}</span>
@@ -44,47 +46,56 @@
       </p>
     {/each}
   </div>
-{/if}
+{/if} -->
 
 <main class="flex justify-evenly flex-wrap">
   <section class="flex flex-col p-4 items-center">
-    <h2 class="text-2xl">Local Weather</h2>
-    <img src={localWeather.img} alt={localWeather.description} />
+    <h2 class="text-2xl">Current Weather</h2>
+    <!-- <img src={localWeather.img} alt={localWeather.description} /> -->
+    <span class="text-8xl">{localWeather.temp}&deg;</span>
     <h3 class="text-xl capitalize">{localWeather.description}</h3>
-    <span>Temperature: {localWeather.temp}&deg;</span>
+    <!-- <span>Feels Like: {localWeather.feels_like}&deg;</span> -->
     <span>Humidity: {localWeather.humidity}%</span>
-    <!-- <span>Wind Speed: {localWeather.wind_speed} mph</span> -->
-    <!-- <span>Wind Direction: {localWeather.wind_deg}&deg;</span> -->
+    <!-- <span>Wind Speed: {localWeather.wind_speed} mph @ {localWeather.wind_deg}&deg;</span>
+    <span>Cloud Coverage: {localWeather.clouds}%</span>
     <span class="text-xs">
       {coords.latitude}&deg; {coords.longitude}&deg;
-    </span>
-    <span />
+    </span> -->
   </section>
-  <section class="flex flex-col p-4 items-center">
+  <!-- <section class="flex flex-col p-4 items-center">
     <h2 class="text-2xl">Home Weather</h2>
-  </section>
+  </section> -->
   <section class="w-full">
     <h2>Hourly Forecast</h2>
-    <div class="flex justify-evenly flex-wrap">
+    <div class="flex items-center overflow-x-auto">
       {#each hourly as hour, index}
         {#if index <= 12}
-          <div class="m-2 flex flex-col items-center">
-            <span
-              >{new Date(parseInt(String(hour.dt) + "000"))
-                .toLocaleTimeString()
-                .split(":")[0]}{new Date(parseInt(String(hour.dt) + "000"))
-                .toLocaleTimeString()
-                .split(" ")[1]}</span
-            >
+          <div class="m-2 flex flex-col items-center justify-center text-sm">
+            <span>{new Date(parseInt(String(hour.dt) + "000")).toLocaleTimeString().split(":")[0]}{new Date(parseInt(String(hour.dt) + "000")).toLocaleTimeString().split(" ")[1]}</span>
+            <img src={`https://openweathermap.org/img/wn/${hour.weather[0].icon}.png`} />
             <span>{Math.round(hour.temp)}&deg;</span>
-            <span>{hour.weather[0].main}</span>
+            <!-- <span>{hour.weather[0].main}</span> -->
           </div>
         {/if}
+      {/each}
+    </div>
+  </section>
+  <section class="w-full">
+    <h2>Daily Forecast</h2>
+    <div class="flex flex-col">
+      {#each daily as day}
+        <div class="flex items-center justify-evenly">
+          <span>{new Date(parseInt(String(day.dt) + "000")).toLocaleDateString()}</span>
+          <img src={`https://openweathermap.org/img/wn/${day.weather[0].icon}.png`} />
+          <span>High: {Math.round(day.temp.max)}&deg;</span>
+          <span>Low: {Math.round(day.temp.min)}&deg;</span>
+          <!-- <span>{day.weather[0].main}</span> -->
+        </div>
       {/each}
     </div>
   </section>
 </main>
 
 <!-- <pre>
-    {JSON.stringify(hourly[0], null, 2)}
+    {JSON.stringify(daily[0], null, 2)}
 </pre> -->
